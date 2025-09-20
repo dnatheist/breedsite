70ec4a3c744ab886369ff1d4c6f2d10095deaf6b
Alan Couch <alan@couch.net.au>
2025-09-21 09:05:40 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-<artifact artifact_id="redpoll-simple-script" artifact_version_id="1" title="script.js" contenttype="text/javascript">
 document.getElementById('donateButton').addEventListener('click', function() {
     alert('Redirecting to donation page... Support Red Poll today!');
     window.open('https://redpoll.au/Donate', '_blank');
 });
-</artifact>
