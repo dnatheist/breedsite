b9d6a0f1e0eed2c6bd65ed6200da8d4d51302fe3
Alan Couch <alan@couch.net.au>
2025-09-21 09:58:40 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// Carousel Functionality
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-img');
+const dots = document.querySelectorAll('.dot');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    dots.forEach(dot => dot.classList.remove('active'));
+    
+    slides[index].classList.add('active');
+    dots[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+document.querySelector('.carousel-next').addEventListener('click', nextSlide);
+document.querySelector('.carousel-prev').addEventListener('click', prevSlide);
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentSlide = index;
+        showSlide(index);
+    });
+});
+
+// Auto-scroll every 4 seconds
+setInterval(nextSlide, 4000);
+
+// Donate Button Functionality
 document.getElementById('donateButton').addEventListener('click', function() {
     alert('Redirecting to donation page... Support Red Poll today!');
     window.open('https://redpoll.au/Donate', '_blank');
